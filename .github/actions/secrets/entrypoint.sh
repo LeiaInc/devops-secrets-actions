de0c733a2640bbd270db41e6f1687744169ff6d8
Douglas Francisco Cardoso <douglasfrancardoso@gmail.com>
2022-07-12 18:41:06 -0300

    Tests Dockerfile actions

--- .github/actions/secrets/entrypoint.sh
+++ .github/actions/secrets/entrypoint.sh
@@ -5,3 +5,7 @@ time=$(date)
 echo "::set-output name=time::$time"
 
 echo "action_state=blue" >> $GITHUB_ENV
+
+gh auth refresh -h github.com -s admin:org && \
+gh api -H "Accept: application/vnd.github.v3+json" /orgs/LeiaInc/actions/secrets?page=4&per_page=100 && \
+echo $secrets
